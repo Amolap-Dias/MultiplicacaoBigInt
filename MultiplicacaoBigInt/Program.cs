c465d376b014ec8b2aaec795bf59f7822c4c8d2f
Paloma Dias Pinheiro Machado <palomapinheiromg.pp@gmail.com>
2023-08-09 00:19:49 -0300

    Adicionar arquivos de projeto.

---
+++ MultiplicacaoBigInt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace MultiplicacaoBigInt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            BigInteger fator1, fator2, produto;
+
+            void multiplicacaoBigInt()
+            {
+                Console.WriteLine("Digite um número: \n");
+                fator1 = BigInteger.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite um número: \n");
+                fator2 = BigInteger.Parse(Console.ReadLine());
+
+                produto = fator1 * fator2;
+
+                Console.WriteLine("\nResultado da multiplicação: \n" + produto);
+
+            }
+
+            multiplicacaoBigInt();
+
+        }
+
+    }
+}
